0787dc7cbe2c29e541f6f87411ad12d03e658756
rc1140 <rc1140@republiccommandos.co.za>
2013-03-21 21:27:49 +0000

    Final checkin for the basic working version of the grouper

--- web.py
+++ web.py
@@ -1,9 +1,48 @@
 from flask import Flask
+from flask import render_template
+from flask import redirect
+from flask import request
+from flask import url_for
+
+import redis
+
+r = redis.Redis()
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+	repoHashes = r.lrange('REPOS',0,-1)
+	tags  = r.lrange('TAGS',0,-1)
+	finalNames = []
+	for rHash in repoHashes[0:10]:
+		finalNames.append({'name':r.get('REPO:%s'%rHash),'hash':rHash,'tags':r.lrange('REPO:%s:TAGS'%rHash,0,-1)})
+		
+	return render_template('index.html',repos=finalNames,tags=tags)
+
+@app.route("/saveTags",methods=['POST'])
+def saveTags():
+	for keyPair in request.form:
+		r.delete('REPO:%s:TAGS'%keyPair)
+		for tag in request.form.getlist(keyPair):
+			r.lpush('REPO:%s:TAGS'%keyPair,tag)
+	return redirect(url_for('index'))
+
+@app.route("/viewTags")
+def viewTags():
+	tags  = r.lrange('TAGS',0,-1)
+	return render_template('tags.html',tags=tags)
+
+@app.route("/viewTag/<tag>")
+def viewTag(tag):
+	repoHashes = r.lrange('REPOS',0,-1)
+	finalRepos = []
+	for rHash in repoHashes[0:10]:
+		repoTags = r.lrange('REPO:%s:TAGS'%rHash,0,-1)
+		if tag in repoTags:
+			finalRepos.append({'name':r.get('REPO:%s'%rHash)})
+		
+	return render_template('tag.html',repos=finalRepos,tag=tag)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0',port=8000,debug=True)
